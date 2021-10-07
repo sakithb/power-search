d9386cde80d68f18ea7515a83370597efc52cb7a
Sakith B <pvsakith@gmail.com>
2021-10-08 03:22:29 +0530

    Hided API keys from config

--- next.config.js
+++ next.config.js
@@ -14,9 +14,4 @@ module.exports = {
 
         return config;
     },
-    env: {
-        SPOTIFY_API_KEY:
-            "BQCYgNTSm6o3tUD1x-VBGH9Nw9o9ZU5xNiz-a-ReuCtMFrka6R2iMvuIaBcz85e472-hc2Ea0kDJAYDDx0ne-GiF649k_omx2DcsuTlIN2t8tYfO89v52zmAk7q9nVdjYw1ApkWsJcMbn3lAuOTSIqs0vFWaLsAn7Jk",
-        GOOGLE_KEY: "AIzaSyDNqYRDsb-JaUan7UdP05alvuGHWCD6NwY",
-    },
 };
